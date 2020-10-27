1d3c58fd8f583890c0f75f37c7e283a3615fd4bf
Kathy Vira <katy2002@gmail.com>
2020-10-27 22:44:22 +0200

    finished blog and specific post

--- blog/models.py
+++ blog/models.py
@@ -8,3 +8,6 @@ class Post(models.Model):
 	date = models.DateTimeField()
 	text = models.TextField()
 	event_image = models.ImageField(upload_to='event_images/')
+
+	def get_summary(self):
+		return self.text[:70]
